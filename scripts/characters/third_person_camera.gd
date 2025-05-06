726ff22133115cb5cf625fa841fb7e1f4eb1a51b
Valdomiro Morais <valdomiromorais@gmail.com>
2025-05-06 17:29:06 -0300

    cmt02 - We imported the Mixamo character (Michele), adjusted the player script for Michele.

--- scripts/characters/third_person_camera.gd
+++ scripts/characters/third_person_camera.gd
@@ -6,10 +6,15 @@ var cam_sensitivity  : float = 0.2
 @export
 var cam_acceleration : float = 10.0
 
+@export
+var spring_length: float = 3.0
+
 @onready
 var horizontal_pivot: Node3D = get_node("HorizontalPivot")
 @onready
 var vertical_pivot: Node3D = get_node("HorizontalPivot/VerticalPivot")
+@onready
+var spring_arm: SpringArm3D = get_node("HorizontalPivot/VerticalPivot/SpringArm3D")
 
 #inner varieble
 var horizontal_degree : float = 0.0
@@ -17,6 +22,10 @@ var vertical_degree   : float = 0.0
 
 const MIN: int = -60
 const MAX: int = +60
+
+func _ready() -> void:
+	spring_arm.spring_length = spring_length
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
@@ -30,6 +39,3 @@ func _physics_process( delta:float ) -> void:
 	vertical_degree = clamp(vertical_degree, MIN, MAX)
 	horizontal_pivot.rotation_degrees.y = lerp( horizontal_pivot.rotation_degrees.y, horizontal_degree, cam_acceleration * delta)
 	vertical_pivot.rotation_degrees.x = lerp( vertical_pivot.rotation_degrees.x, vertical_degree, cam_acceleration * delta)
-
-func _ready() -> void:
-	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
--- scripts/characters/third_person_controller.gd
+++ scripts/characters/third_person_controller.gd
@@ -7,10 +7,10 @@ var speed: float = 2.0
 var jump_strength: float = 4.5
 
 @onready var cam_horizontal_pivot: Node3D = get_node("CameraSystem/HorizontalPivot")
-#@onready var armature_node: Node3D = get_node("Node") # ESTE É o CORRETO
-@onready var mesh: MeshInstance3D = get_node("Mesh")
-
-
+@onready var mesh: Node3D = get_node("Node") # ESTE É o CORRETO qdo tivermos um personagem
+# @onready var mesh: MeshInstance3D = get_node("Mesh")
+@onready
+var animation: AnimationPlayer = get_node("AnimationPlayer")
 
 
 func _physics_process(delta):
@@ -18,16 +18,21 @@ func _physics_process(delta):
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = jump_strength
 
-	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction = (transform.basis * Vector3(input_dir.x,0,input_dir.y) ).normalized().rotated(Vector3.UP,hor_rotation)
+	var input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_back")
+	var direction = (transform.basis * Vector3(input_dir.x,0,input_dir.y)).normalized().rotated(Vector3.UP,hor_rotation)
+	
 	if direction:
+		# em movimento
+		animation.play("Michele_Walk")
 		velocity.x = direction.x * speed
 		velocity.z = direction.z * speed
 		mesh.rotation.y = lerp_angle( mesh.rotation.y, atan2( direction.x, direction.z ), delta * 5)
 	else:
+		# em repouso
+		animation.play("Michele_Idle")
 		velocity.x = move_toward(velocity.x, 0, speed)
 		velocity.z = move_toward(velocity.z, 0, speed)
 	move_and_slide()
